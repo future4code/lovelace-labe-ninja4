1ecf0cc9258c1ac67bdc72383e00e2a3f2d24331
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-08-05 22:06:56 -0300

    Correção do componente Header

--- src/components/Header/EstilosHeader.js
+++ src/components/Header/EstilosHeader.js
@@ -1,37 +1,33 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const BarraDeNavegacao = styled.div`
-    background-color: #8265c9;
-    box-shadow: 10px 5px 5px grey;
-    color: white;
-    padding: 5px 20px;
-    display: flex;
-    justify-content: space-between;
+export const HeaderContainer = styled.div`
+  box-sizing: border-box;
+  background: #bbb;
+  display: flex;
+  justify-content: space-between;
+`;
 
-`
-export const Links = styled.a`
-    display: block;
-    grid-template-columns: repeat(5, auto);
-    color: white;
+export const Logo = styled.div`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  img {
+    width: 88px;
+  }
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  align-items: center;
+
+  a {
+    margin-right: 16px;
     text-decoration: none;
-`
-export const LogoETitulo = styled.div`
-    display: flex;
-`
-
-export const Logo = styled.img`
-    width: 50px;
-    height: 50px;
-    margin-right: 10px;
-    margin-top: 20px;
-`
-
-export const IconeMenu = styled.div`
-    display:block;
-    font-size: 1.8rem;
-    position: absolute;
-    top: 0;
-    right: 0;
-    cursor: pointer;
-    transform: translate(-100%, 60%)
-`
+    padding: 6px;
+    color: #fff;
+    font-size: 1.2rem;
+    :hover {
+      color: #000;
+    }
+  }
+`;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,21 @@
 import React from "react";
 import logo from "../Header/logo.png";
-import { BarraDeNavegacao } from "./EstilosHeader";
-import { Links } from "./EstilosHeader";
-import { LogoETitulo } from "./EstilosHeader";
-import { Logo } from "./EstilosHeader";
-
-const ItensMenu = [
-  {
-    titulo: "Home",
-    endereço: "#",
-    nomeclasse: "linksnevegacao",
-  },
-  {
-    titulo: "Carrinho",
-    endereço: "#",
-    nomeclasse: "linksnevegacao",
-  },
-];
+import { HeaderContainer, Menu, Logo } from "./EstilosHeader";
 
 export default class Header extends React.Component {
   render() {
     return (
-      <div>
-        <BarraDeNavegacao>
-          <LogoETitulo>
-            <Logo src={logo} alt="logo da Labeninjas" />
-            <h1>LabeNinjas</h1>
-          </LogoETitulo>
+      <HeaderContainer>
+        <Logo>
+          <img src={logo} alt="logo da Labeninjas" />
+          <h1>Labeninjas</h1>
+        </Logo>
 
-          <div>
-            <ul>
-              {ItensMenu.map((item, index) => {
-                return (
-                  <div key={index}>
-                    <Links className={item.nomeclasse} href={item.endereço}>
-                      {item.titulo}
-                    </Links>
-                  </div>
-                );
-              })}
-            </ul>
-          </div>
-        </BarraDeNavegacao>
-      </div>
+        <Menu>
+          <a href="/">Home</a>
+          <a href="/">Carrinho</a>
+        </Menu>
+      </HeaderContainer>
     );
   }
 }
