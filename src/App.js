471fef5f8cb285befd26f51f78c8e6ab3d53fcca
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-08-19 09:32:13 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,25 @@
-import React from "react";
-import Header from "./components/Header/Header";
-import HomePage from "./components/Main/HomePage";
-import CreateServicePage from "./components/Main/CreateServicePage";
-import CartPage from "./components/Main/CartPage";
-import ServiceListPage from "./components/Main/ServiceListPage";
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends React.Component {
-  state = {
-    currentPage: "homepage",
-  };
-
-  goToServiceListPage = () => this.setState({ currentPage: "servicelist" });
-
-  goToCreateServicePage = () => this.setState({ currentPage: "servicepage" });
-
-  goToCartPage = () => this.setState({ currentPage: "cartpage" });
-
-  goToHomePage = () => this.setState({ currentPage: "homepage" });
-
-  selectPage = () => {
-    switch (this.state.currentPage) {
-      case "homepage":
-        return (
-          <HomePage
-            goToServiceListPage={this.goToServiceListPage}
-            goToCreateServicePage={this.goToCreateServicePage}
-          />
-        );
-      case "servicepage":
-        return <CreateServicePage goToHomePage={this.goToHomePage} />;
-      case "cartpage":
-        return <CartPage goToServiceListPage={this.goToServiceListPage} />;
-      case "servicelist":
-        return <ServiceListPage />;
-      default:
-        return <HomePage />;
-    }
-  };
-
-  render() {
-    return (
-      <div>
-        <Header
-          goToHomePage={this.goToHomePage}
-          goToCartPage={this.goToCartPage}
-        />
-        {this.selectPage()}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
